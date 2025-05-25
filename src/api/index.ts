dd27d61e01ae70e5554dd04f2618c99ca4feecb3
Raina Moon <mds6425@gmail.com>
2025-05-25 21:26:54 +0900

    ✨ add: add admin router path in API index file

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,10 @@
-import express from 'express';
-import authRouter from './auth';
+import express from "express";
+import authRouter from "./auth";
+import adminRouter from "./admin";
 
 const router = express.Router();
 
-router.use('/auth', authRouter);
+router.use("/auth", authRouter);
+router.use("admin", adminRouter);
 
 export default router;
