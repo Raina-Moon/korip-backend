ec378d82e4c7d33f91868facf0b546939bc952b9
Raina Moon <mds6425@gmail.com>
2025-05-27 19:52:01 +0900

    ✨feat : add reset password route to the main API router

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import authRouter from "./auth";
 import adminRouter from "./admin";
 import socialLoginRouter from "./socialLogin";
+import resetPasswordRouter from "./reset-password";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/admin", adminRouter);
 router.use("/social-login", socialLoginRouter);
+router.use("/reset-password", resetPasswordRouter);
 
 export default router;
