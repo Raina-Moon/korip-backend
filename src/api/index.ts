e3a0abf89c67422101efda2e861dd3792a122f02
Raina Moon <mds6425@gmail.com>
2025-05-27 15:17:24 +0900

    ✨ feat: add social login router to API routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import authRouter from "./auth";
 import adminRouter from "./admin";
+import socialLoginRouter from "./socialLogin";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/admin", adminRouter);
+router.use("/social-login", socialLoginRouter);
 
 export default router;
