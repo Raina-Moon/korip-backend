503b791c51162fc7ffb3eb8d3f9402f9e05ae4b9
Raina Moon <mds6425@gmail.com>
2025-05-30 20:01:30 +0900

    👽️ refactor: improve code formatting and consistency in fetchHotspringData function

--- src/services/hotspringService.ts
+++ src/services/hotspringService.ts
@@ -1,37 +1,39 @@
-import axios from "axios"
+import axios from "axios";
 
-export const fetchHotspringData = async (sido?:string) => {
-    const url =  "https://api.vworld.kr/req/data"
+export const fetchHotspringData = async (sido?: string) => {
+  const url = "https://api.vworld.kr/req/data";
 
-    const params: any = {
-        service : "data",
-        version : "2.0",
-        request : "GetFeature",
-        key : process.env.VWORLD_API_KEY,
-        data : "LT_C_ADM_SECT_UMD",
-        domain : process.env.VWORLD_DOMAIN || "http://localhost:3000",
-        format : "json",
-        crs: "EPSG:4326",
-        size: 10,
-        page:1,
-        geometry: true,
-        attribute : true,
-        buffer : 0  
-    }
+  const params: any = {
+    service: "data",
+    version: "2.0",
+    request: "GetFeature",
+    key: process.env.VWORLD_API_KEY,
+    data: "LT_C_ADM_SECT_UMD",
+    domain: process.env.VWORLD_DOMAIN || "http://localhost:3000",
+    format: "json",
+    crs: "EPSG:4326",
+    size: 10,
+    page: 1,
+    geometry: true,
+    attribute: true,
+    buffer: 0,
+  };
 
-    if (sido) {
-        params.attrFilter = `sido_name:=:${sido}`
+  if (sido) {
+    params.attrFilter = `sido_name:LIKE:${sido}`;
+  }
+
+  try {
+    const response = await axios.get(url, { params });
+    const features =
+      response.data?.response?.result?.featureCollection?.features;
+    if (!features) {
+      return features;
+    } else {
+      throw new Error("Invalid response structure");
     }
-    
-    try {
-        const response = await  axios.get(url, {params})
-        if (response.data.response && response.data.response.result) {
-            return response.data.response.result.features
-        } else {
-            throw new Error("Invalid response structure")
-        }
-    } catch (error) {
-        console.error("Error fetching hotspring data:", error)
-        throw error
-    }
-}
+  } catch (error) {
+    console.error("Error fetching hotspring data:", error);
+    throw error;
+  }
+};
