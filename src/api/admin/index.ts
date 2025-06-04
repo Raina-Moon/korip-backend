d1ec1a4bacb4e7db636151cb9e5a7bb462942f8a
Raina Moon <mds6425@gmail.com>
2025-06-04 17:14:39 +0900

    ✨ feat: add room pricing and room type routes to admin API

--- src/api/admin/index.ts
+++ src/api/admin/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import userRouter from "./user";
 import lodgeRouter from "./lodge";
+import roomPricingRouter from "./roomPricing";
+import roomTypeRouter from "./roomType";
 import { authToken } from "../../middlewares/authMiddleware";
 import { isAdmin } from "../../middlewares/adminMiddleware";
 
@@ -10,5 +12,7 @@ router.use(authToken, isAdmin);
 
 router.use("/user", userRouter);
 router.use("/lodge", lodgeRouter);
+router.use("/room-pricing",roomPricingRouter);
+router.use("/room-type", roomTypeRouter);
 
 export default router;
