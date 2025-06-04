9876bc6812768e2d9806b85899b05af161619dff
Raina Moon <mds6425@gmail.com>
2025-06-04 17:53:52 +0900

    ✨ feat: add room inventory router to admin API

--- src/api/admin/index.ts
+++ src/api/admin/index.ts
@@ -3,6 +3,7 @@ import userRouter from "./user";
 import lodgeRouter from "./lodge";
 import roomPricingRouter from "./roomPricing";
 import roomTypeRouter from "./roomType";
+import roomInventoryRouter from "./roomInventory";
 import { authToken } from "../../middlewares/authMiddleware";
 import { isAdmin } from "../../middlewares/adminMiddleware";
 
@@ -14,5 +15,6 @@ router.use("/user", userRouter);
 router.use("/lodge", lodgeRouter);
 router.use("/room-pricing",roomPricingRouter);
 router.use("/room-type", roomTypeRouter);
+router.use("/room-inventory", roomInventoryRouter);
 
 export default router;
