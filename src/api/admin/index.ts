c1086295652b5a2707a668efced9cbad61949a7e
Raina Moon <mds6425@gmail.com>
2025-06-25 00:01:01 +0900

    ✨ feat: add reservation router to admin API

--- src/api/admin/index.ts
+++ src/api/admin/index.ts
@@ -4,6 +4,7 @@ import lodgeRouter from "./lodge";
 import roomTypeRouter from "./roomType";
 import roomInventoryRouter from "./roomInventory";
 import reportsRouter from "./reports";
+import reservationRouter from "./reservation";
 import { authToken } from "../../middlewares/authMiddleware";
 import { isAdmin } from "../../middlewares/adminMiddleware";
 
@@ -15,6 +16,7 @@ router.use("/user", userRouter);
 router.use("/lodge", lodgeRouter);
 router.use("/room-type", roomTypeRouter);
 router.use("/room-inventory", roomInventoryRouter);
-router.use("/reports",reportsRouter)
+router.use("/reports",reportsRouter);
+router.use("/reservation", reservationRouter);
 
 export default router;
